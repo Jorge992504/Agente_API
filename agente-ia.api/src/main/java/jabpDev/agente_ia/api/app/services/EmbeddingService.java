dead27232c381fdfc9f833868755a947beefc756
jorge.prado <jorge@hs-software.dev.br>
2026-07-06 17:41:38 -0300

    ajustada a classe embiddingsewrvice

--- agente-ia.api/src/main/java/jabpDev/agente_ia/api/app/services/EmbeddingService.java
+++ agente-ia.api/src/main/java/jabpDev/agente_ia/api/app/services/EmbeddingService.java
@@ -2,14 +2,16 @@ package jabpDev.agente_ia.api.app.services;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.ai.ollama.OllamaEmbeddingModel;
+import org.springframework.ai.embedding.EmbeddingModel;
+//import org.springframework.ai.ollama.OllamaEmbeddingModel;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class EmbeddingService {
 
-    private OllamaEmbeddingModel embeddingModel;
+//    private OllamaEmbeddingModel embeddingModel;
+    private EmbeddingModel embeddingModel;
 
     public float[] gerarEmbedding(String text){
         return embeddingModel.embed(text);
