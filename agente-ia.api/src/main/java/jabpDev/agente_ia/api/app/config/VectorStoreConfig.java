9db790b261cd71ce93040a1752a24038e84cc199
jorge.prado <jorge@hs-software.dev.br>
2026-07-07 11:31:16 -0300

    comentado os embedding

--- agente-ia.api/src/main/java/jabpDev/agente_ia/api/app/config/VectorStoreConfig.java
+++ agente-ia.api/src/main/java/jabpDev/agente_ia/api/app/config/VectorStoreConfig.java
@@ -1,13 +1,18 @@
 package jabpDev.agente_ia.api.app.config;
 
 import com.openai.models.vectorstores.VectorStore;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class VectorStoreConfig {
@@ -22,11 +27,30 @@ public class VectorStoreConfig {
 //    }
 
     @Bean
-    public PgVectorStore vectorStore(
-            JdbcTemplate jdbcTemplate,
-            EmbeddingModel embeddingModel) {
+    @Primary
+    public PgVectorStore vectorStore() {
 
-        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
-                .build();
+        PgVectorStore pgVectorStore = new PgVectorStore() {
+            @Override
+            public void add(List<Document> documents) {
+                // Mock: não faz nada
+            }
+
+            @Override
+            public void accept(List<Document> documents) {
+                // Mock: não faz nada
+            }
+
+            @Override
+            public List<Document> similaritySearch(String query) {
+                return Collections.emptyList(); // Retorna vazio sem dar erro
+            }
+
+            @Override
+            public List<Document> similaritySearch(SearchRequest request) {
+                return Collections.emptyList(); // Retonar vazio sem dar erro
+            }
+        };
+        return pgVectorStore;
     }
 }
