7998f40d446e05ca933917e0c2e7d4566e0859d4
jorge.prado <jorge@hs-software.dev.br>
2026-07-07 10:21:17 -0300

    adicionado o teste do embediding

--- agente-ia.api/src/main/java/jabpDev/agente_ia/api/app/config/VectorStoreConfig.java
+++ agente-ia.api/src/main/java/jabpDev/agente_ia/api/app/config/VectorStoreConfig.java
@@ -1,11 +1,13 @@
 package jabpDev.agente_ia.api.app.config;
 
 import com.openai.models.vectorstores.VectorStore;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.transformers.TransformersEmbeddingModel;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
@@ -23,9 +25,24 @@ public class VectorStoreConfig {
     @Bean
     public PgVectorStore vectorStore(
             JdbcTemplate jdbcTemplate,
-            TransformersEmbeddingModel embeddingModel) {
+            EmbeddingModel embeddingModel) {
 
         return PgVectorStore.builder(jdbcTemplate, embeddingModel)
                 .build();
+    }
+
+
+    @Bean
+    @Primary
+    public EmbeddingModel localEmbeddingModel() {
+        // Criando explicitamente o modelo local do Transformers
+        TransformersEmbeddingModel embeddingModel = new TransformersEmbeddingModel();
+        try {
+            // Isso força o Spring AI a rodar o processo de inicialização do ONNX imediatamente
+            embeddingModel.afterPropertiesSet();
+        } catch (Exception e) {
+            throw new IllegalStateException("Falha crítica ao inicializar o modelo ONNX local: " + e.getMessage(), e);
+        }
+        return embeddingModel;
     }
 }
